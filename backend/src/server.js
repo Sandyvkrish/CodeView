9bcc7c04d1855165a9c2ec35e06c75addc5eec66
sandyvkrish <sandyvkrish@gmail.com>
2026-05-03 18:59:28 +0530

    intial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {ENV} from "./lib/env.js";
+
+
+const app = express();
+console.log(ENV.PORT);
+
+app.get("/health ", (req,res) => {
+    res.status(200).json({msg:"success from api"})
+});
+app.listen(ENV.PORT, () => console.log("server is running port 9090"));
