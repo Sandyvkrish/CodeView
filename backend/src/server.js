c344b77c0b3c56fad4abe7279cfc034606b01b43
sandyvkrish <sandyvkrish@gmail.com>
2026-05-08 16:03:55 +0530

    implementing server side auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,11 @@ import path from "path";
 
 import { ENV } from "./lib/env.js";  
 import { connectDB } from "./lib/db.js";  
+import { clerkMiddleware } from '@clerk/express';
 
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js"; 
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -15,15 +17,15 @@ const __dirname= path.resolve();
 app.use(express.json());
 
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(clerkMiddleware()) //this adds auth field to request object : req.auth()
 
-app.use("/api/inngest", serve({client:inngest, functions }))
+app.use("/api/inngest", serve({client:inngest, functions }));
+app.use(clerkMiddleware());
 
 app.get("/health", (req,res) => {
     res.status(200).json({msg:"success from api"});
 });
-app.get("/books", (req,res) => {
-    res.status(200).json({msg:"this is books endpoint"});
-});
+
 //making deployment ready
 if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
